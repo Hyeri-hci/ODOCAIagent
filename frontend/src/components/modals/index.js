bdd14323ac110bf041c40502aeb91a482577e9db
Hyeri-hci <hyeri.hci.du@gmail.com>
2025-12-11 10:42:54 +0900

    [refactor] 사용되지 않는 테스트 파일과 UI 컴포넌트 제거하여 프로젝트 정리

--- frontend/src/components/modals/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ConfirmSendModal } from "./ConfirmSendModal";
-export { default as KakaoLoginModal } from "./KakaoLoginModal";
-export { default as LoadingOverlay } from "./LoadingOverlay";
