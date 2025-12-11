1fffd6c7f7de54475b2812d1e6562a691e393ae1
Hyeri-hci <hyeri.hci.du@gmail.com>
2025-12-11 10:45:42 +0900

    [feat] Tailwind CSS 애니메이션 및 다크 모드 설정 추가, .gitignore에 테스트 및 문서 디렉토리 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // React 관련 라이브러리 분리
+          "vendor-react": ["react", "react-dom", "react-router-dom"],
+          // 기타 외부 라이브러리 분리
+          "vendor-utils": ["axios"],
+        },
+      },
+    },
+  },
+});
